cc1293ef43a97594397b98b802e34b3c43880ec3
Wayan Santie Arif <64580513+wyanns-404@users.noreply.github.com>
2022-10-15 18:51:48 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
-
 using namespace std;
-
 int main(){
-
-    cout<<"Hello World"<<endl;
-    cout<<"cobaaaaaa"<<endl;
-
-
-    return 0;
+	string nama,npm,jurusan;
+	int umur,thn;
+	
+	cout<<"";
+	getline(cin,nama);
+	cout<<"";
+	getline(cin,npm);
+	cout<<"";
+	getline(cin,jurusan);
+	cout<<"";
+	cin>>thn;
+	umur=2022-thn;
+	
+	cout<<"Nama Saya \""<<nama<<"\","<<endl;
+	cout<<"umur saya \""<<umur<<" \Tahun"<<"\","<<endl;
+	cout<<"dengan NPM \""<<npm<<"\","<<endl;
+	cout<<"dari Jurusan "<<jurusan<<endl;
+	
+	
+	
+	
+	
+	return 0;
 }
