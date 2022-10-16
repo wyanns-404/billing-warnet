6106667ea37254cfd2dc2de4d8c66a627c367df2
Wayan Santie Arif <64580513+wyanns-404@users.noreply.github.com>
2022-10-16 13:07:27 +0700

    Update Halaman Utama

--- billing-warnet.cpp
+++ billing-warnet.cpp
@@ -4,9 +4,26 @@ using namespace std;
 
 int main(){
 
+	string HalamanUtama;
+	int HalamanUtama0;
 
+	cout<<"==========Selamat Datang di Warnet SansSkuy==========\n\n";
 
+	cout<<"Menu Paketan :\n";
+	cout<<"1. Open\n";
+	cout<<"2. Perjam\n";
+	cout<<"3. Paket Malam\n\n";
 
+	cout<<"Keterangan :\n";
+	cout<<"a. Open dihitung dengan berapa lama pengguna memakai\n";
+	cout<<"b. Perjam : Rp 5.000\n";
+	cout<<"c3. Paket malam dimulai dari jam 23.00 - 05.00\n\n";
 
-    return 0;
+	cout<<"Masukan pilihan anda 1;2;3 = ";
+	cin>>HalamanUtama0;
+	
+	
+	
+	
+	return 0;
 }
